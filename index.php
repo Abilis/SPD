b2db96afa8c3304e7e4576e12d3a9d26b41bb6f5
Abilis <abilis@mail.ru>
2016-02-18 16:58:28 +0400

    Меню навигации с кодом обсчета вынесено в отдельный шаблон

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+ 
+//error_reporting(E_ERROR);
 
 require_once('database.php');
 require_once('functions.php');
@@ -9,17 +11,21 @@ $link = startup();
 /*//вытаскиваем все записи из БД
 $entries = get_entryes_all($link);*/
 
-//Вытаскиваем последние записи из БД для постраничной навигации
+//Вытаскиваем записи из БД для постраничной навигации
 $entries_arr = get_entries_num_start($link);
 
 //Разрираем полученный массив. Подробнее в functions.php
 $entries = $entries_arr[0];
-$page = $entries_arr[1];
-$total = $entries_arr[2];
+$page = $entries_arr[1]; //текущая страница
+$total = $entries_arr[2]; //всего страниц
     
 //Выводим в шаблоны
 include('views/v-header.php');
 include('views/v-index.php');
+
+//подключаем нижнее меню навигации
+include('menu_navigation.php');
+
 include('views/v-footer.php');
 
 ?>
--- views/v-index.php
+++ views/v-index.php
@@ -78,38 +78,3 @@
     <?php endforeach ?>
 
 </table>
-
-<?php
-  
-    //Проверяем, нужны ли стрелки назад
-    if ($page != 1) { 
-        $pervpage = '<a href= ./index.php?page=1><<</a> <a href= ./index.php?page=' . ($page - 1) . '><</a> ';
-    }
-                
-    // Проверяем нужны ли стрелки вперед 
-    if ($page != $total) {
-        $nextpage = ' <a href= ./index.php?page=' . ($page + 1) . '>></a> <a href= ./index.php?page=' . $total . '>>></a>';
-    }
-                
-
-    // Находим две ближайшие станицы с обоих краев, если они есть 
-    if ($page - 2 > 0) {
-        $page2left = ' <a href= ./index.php?page=' . ($page - 2) . '>' . ($page - 2) . '</a> | ';
-    }
-                
-    if ($page - 1 > 0) {
-        $page1left = '<a href= ./index.php?page=' . ($page - 1) . '>' . ($page - 1) . '</a> | ';}
-                
-    if ($page + 2 <= $total) {
-        $page2right = ' | <a href= ./index.php?page=' . ($page + 2) . '>' . ($page + 2) . '</a>';
-    }
-                
-    if ($page + 1 <= $total) {
-        $page1right = ' | <a href= ./index.php?page=' . ($page + 1) . '>' . ($page + 1) . '</a>';
-     }
-
-    
-    // Вывод меню 
-    echo $pervpage . $page2left . $page1left . '<b>' . $page . '</b>' . $page1right . $page2right . $nextpage; 
-
-?>
