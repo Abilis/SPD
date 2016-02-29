181a470cd41c48415f0aa4e08156f1550ec822bc
Abilis <abilis@mail.ru>
2016-02-29 19:09:27 +0400

    Теперь вход и выход с цмс разделены более логично

--- views/v-add.php
+++ views/v-add.php
@@ -1,10 +1,10 @@
-<a href="login.php">Войти</a>
 <?php if ($user != null) { ?>
-            <br />Вы вошли как <b><?=$user['login']?></b>
+            <br />Вы вошли как <b><?=$user['login']?></b><br />
+            <a href="logout.php">Выйти</a>
 <?php } ?>
 
 <?php if ($user == null) { ?>
-            <br />Вы не залогинены</b>
+            <br /><b>Вы не залогинены</b>
 <?php } ?><br /><br />
 
 <a href="index.php">Главная</a> <b>|</b> <a href="all_entries.php">Показать все</a> <b>|</b> <a href="add.php">Добавить клиента</a>
