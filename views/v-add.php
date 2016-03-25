b7b8b9c6b1d86e011122558172a303464a6cff74
Abilis <abilis@mail.ru>
2016-03-25 09:29:05 +0400

    Уменьшен шрифт подсказок обязательно/необязательно при добавлении записи. Увеличено поле порта клиента для добавления клиента.

--- views/v-add.php
+++ views/v-add.php
@@ -15,35 +15,35 @@
     <form action="add.php" method="post">
         <tr>
             <td><input id="addNumOrder" class="addForm" type="text" name="numOrder" value="<?=$numOrder?>" /> <br />
-                <div class="italic">Рек.</div>
+                <div class="italicSmall">Рек.</div>
             </td>
             <td><input class="addForm" type="text" name="customer" value="<?=$customer?>"/> <br />
-                <div class="bold">Обязательное поле!</div>
+                <div class="boldSmall">Обязательное поле!</div>
             </td>
             <td><input class="addForm" type="text" name="tarif" value="<?=$tarif?>"/> <br />
-                <div class="italic">Рекомендуется к заполнению</div>
+                <div class="italicSmall">Рекомендуется к заполнению</div>
             </td>
             <td><input class="addForm" type="text" name="ip_address" value="<?=$ip_address?>"/> <br />
-                <div class="bold">Обязательное поле!</div>
+                <div class="boldSmall">Обязательное поле!</div>
             </td>
             <td><input class="addForm" type="text" name="netmask" value="<?=$netmask?>"/> <br />
-                <div class="italic">Рекомендуется к заполнению</div>
+                <div class="italicSmall">Рекомендуется к заполнению</div>
             </td>
             <td><input class="addForm" type="text" name="gateway" value="<?=$gateway?>"/> <br />
-                <div class="italic">Рекомендуется к заполнению</div>
+                <div class="italicSmall">Рекомендуется к заполнению</div>
             </td>
             <td><input id="addVlanId" class="addForm" type="text" name="vlan_id" value="<?=$vlan_id?>"/> <br />
-                <div class="bold">Обяз!</div>
+                <div class="boldSmall">Обяз!</div>
             </td>
-            <td><input class="addForm" type="text" name="customer_port" value="<?=$customer_port?>"/> <br />
-                <div class="italic">Рекомендуется к заполнению</div>
+            <td><input class="addFormCustomer" type="text" name="customer_port" value="<?=$customer_port?>"/> <br />
+                <div class="italicSmall">Рекомендуется к заполнению</div>
             </td>
             <td><input class="addForm" type="text" name="termination_point" value="<?=$termination_point?>"/> <br />
-                <div class="italic">Рекомендуется к заполнению</div>
+                <div class="italicSmall">Рекомендуется к заполнению</div>
             </td>
             
             <td><textarea id="addCommentary" class="addForm" type="text" name="commentary" rows="4"><?=$commentary?></textarea> <br />
-                <div class="italic">Рекомендуется к заполнению</div>
+                <div class="italicSmall">Рекомендуется к заполнению</div>
             </td>
         </tr>
         <br />
